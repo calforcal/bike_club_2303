a1402f0817f9a5299039bb0b8abdd4e2909f7ff7
Michael Callahan <mcalla242@gmail.com>
2023-04-25 09:11:58 -0600

    Feat: Add initialize, loop and total_distance methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,18 @@
 class Ride
+  attr_reader :name, :distance, :terrain, :is_loop
 
+  def initialize(details)
+    @name = details[:name]
+    @distance = details[:distance]
+    @is_loop = details[:loop]
+    @terrain = details[:terrain]
+  end
+
+  def loop?
+    @is_loop
+  end
+
+  def total_distance
+    if loop? then return distance else return distance * 2 end
+  end
 end
