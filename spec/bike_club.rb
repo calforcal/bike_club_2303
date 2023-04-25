4c5c2218fd6ce1c5f8fef1e11605c0e4d2118f68
Michael Callahan <mcalla242@gmail.com>
2023-04-25 10:06:45 -0600

    Test: Add testing specs for bike_club class

---
+++ spec/bike_club.rb
@@ -0,0 +1,62 @@
+require "./lib/ride"
+require "./lib/biker"
+require "./lib/bike_club"
+
+RSpec.describe BikeClub do
+  before(:each) do
+    @club = BikeClub.new("Thank Gravel It's Friday")
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @biker3 = Biker.new("Bicycle Micycle", 100)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+    @ride3 = Ride.new({name: "Flagstaff", distance: 6.8, loop: false, terrain: :hills})
+    @ride4 = Ride.new({name: "Switzerland Trail", distance: 43.6, loop: true, terrain: :gravel})
+
+    @biker1.learn_terrain!(:gravel)
+    @biker1.learn_terrain!(:hills)
+    @biker2.learn_terrain!(:gravel)
+    @biker2.learn_terrain!(:hills)
+    @biker3.learn_terrain!(:gravel)
+    @biker3.learn_terrain!(:hills)
+  end
+
+  describe "#initialize" do
+    it "initializes" do
+      expect(@club).to be_an(BikeClub)
+    end
+
+    it "initializes with attributes" do
+      expect(@club.name).to eq("Thank Gravel It's Friday")
+    end
+  end
+
+  describe "#add_biker" do
+    it "starts with no bikers and can add them" do
+      expect(@club.bikers).to eq([])
+
+      @club.add_biker(@biker1)
+      @club.add_biker(@biker2)
+
+      expect(@club.bikers).to eq([@biker1, @biker2])
+    end
+  end
+
+  describe "#most_rides" do
+    it "can determine the rider with the most rides" do
+      @biker1.log_ride(@ride1, 92.5)
+      @biker1.log_ride(@ride1, 91.1)
+      @biker1.log_ride(@ride2, 60.9)
+
+      @bike2.log_ride(@ride1, 92.5)
+      @bike2.log_ride(@ride1, 91.1)
+
+      @biker3.log_ride(@ride1, 92.5)
+      @biker3.log_ride(@ride1, 91.1)
+      @biker3.log_ride(@ride2, 60.9)
+      @biker3.log_ride(@ride2, 61.6)
+
+      expect(@club.most_rides).to eq(@biker3)
+    end
+  end
+end
