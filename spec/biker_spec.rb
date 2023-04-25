8d6f46ef78a3ab98444974019ac6a295392d157b
Michael Callahan <mcalla242@gmail.com>
2023-04-25 10:51:18 -0600

    Iteration 3 complete

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -33,17 +33,18 @@ RSpec.describe Biker do
 
   describe "#rides && #log_ride" do
     it "can log rides and keep track of rides" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
       @biker.log_ride(@ride2, 60.9)
       @biker.log_ride(@ride2, 61.6)
 
-      expect(@biker.rides).to eq({@ride1 => [92.5, 91.1]})
-      expect(@biker.rides).to eq({@ride2 => [60.9, 61.6]})
+      expect(@biker.rides).to eq({@ride1 => [92.5, 91.1], @ride2 => [60.9, 61.6]})
     end
     
     it "won't log a ride if the rider doesn't know the ride terrain" do
-      @biker2 = biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride1, 97.0)
       @biker2.log_ride(@ride2, 67.0)
 
       expect(@biker2.rides).to eq({})
@@ -56,7 +57,7 @@ RSpec.describe Biker do
       @biker2.learn_terrain!(:gravel)
       @biker2.learn_terrain!(:hills)
 
-      @biker2 = biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride1, 95.0)
       @biker2.log_ride(@ride2, 65.0)
 
       expect(@biker2.rides).to eq({@ride2 => [65.0]})
@@ -65,6 +66,8 @@ RSpec.describe Biker do
 
   describe "#personal_record" do
     it "can return the personal record for a given ride" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
       @biker.log_ride(@ride2, 60.9)
@@ -78,8 +81,8 @@ RSpec.describe Biker do
       @biker2.learn_terrain!(:gravel)
       @biker2.learn_terrain!(:hills)
 
-      @biker2 = biker2.log_ride(@ride1, 95.0)
-      @biker2.log_ride(ride2, 65.0)
+      @biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride2, 65.0)
 
       expect(@biker2.personal_record(@ride1)).to be(false)
       expect(@biker2.personal_record(@ride2)).to eq(65.0)
